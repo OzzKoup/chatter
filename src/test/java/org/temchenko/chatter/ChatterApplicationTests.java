f6b266d6a40cfcab048c3b2076e509e956b252bc
Temchenko <yaroslav20202@gmail.com>
2024-11-29 22:02:53 +0200

    init

---
+++ src/test/java/org/temchenko/chatter/ChatterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.temchenko.chatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
